13b222240606664ef56b42566b542852fa649011
Sean Austin <sean@restaurant-live.co.uk>
2018-11-19 10:17:18 +0000

    added pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { Page01Component } from './page01/page01.component';
+import { Page02Component } from './page02/page02.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page01Component,
+    Page02Component
   ],
   imports: [
     BrowserModule
