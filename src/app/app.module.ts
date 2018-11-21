327db0aed60d9d85ad907e22d0b4e93d4059c143
Sean Austin <sean@restaurant-live.co.uk>
2018-11-21 10:58:04 +0000

    Added Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { Page01Component } from './page01/page01.component';
 import { Page02Component } from './page02/page02.component';
@@ -14,7 +13,12 @@ import { Page02Component } from './page02/page02.component';
     Page02Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'page01', component: Page01Component},
+      {path: 'page02', component: Page02Component}
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
